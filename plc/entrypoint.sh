e516ad57218c51ebebeabc43553c40dfb7f41ab1
Matt Lloyd Davies <103773518+Turzum@users.noreply.github.com>
2025-01-18 16:59:30 +0000

    Add files via upload

---
+++ plc/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Add default route via the router
+#ip route del default via 192.168.2.254
+#ip route add default via 192.168.2.1
+ip route add 192.168.3.0/24 via 192.168.2.254
+
+# Run the main application process
+exec "$@"
