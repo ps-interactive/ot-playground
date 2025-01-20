50323953a37097df2e47b02f2fc07cd6170ca690
Matt Lloyd Davies <103773518+Turzum@users.noreply.github.com>
2025-01-20 19:14:51 +0000

    Update entrypoint.sh

--- scada/entrypoint.sh
+++ scada/entrypoint.sh
@@ -4,6 +4,7 @@
 #ip route del default via 192.168.3.254
 #ip route add default via 192.168.3.1
 ip route add 192.168.2.0/24 via 192.168.3.254
+ip route add 172.31.24.0/24 via 192.168.3.254
 
 # Run the main application process
 exec "$@"
