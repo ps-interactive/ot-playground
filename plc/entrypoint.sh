cc876d77afc78e434a3e1ef14f95fcfadb312b69
Matt Lloyd Davies <103773518+Turzum@users.noreply.github.com>
2025-01-20 19:19:42 +0000

    Update entrypoint.sh

--- plc/entrypoint.sh
+++ plc/entrypoint.sh
@@ -4,7 +4,6 @@
 #ip route del default via 192.168.2.254
 #ip route add default via 192.168.2.1
 ip route add 192.168.3.0/24 via 192.168.2.254
-ip route add 172.31.24.0/24 via 192.168.2.254
 
 # Run the main application process
 exec "$@"
